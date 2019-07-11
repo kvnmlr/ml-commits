5d02605d556dace0dd88d04b3fb45ad6969a7d99
kvnmlr <kevin.mueller1@live.de>
2019-07-11 17:42:30 +0200

    filter commit and work with credentials

--- crawler.py
+++ crawler.py
@@ -1,66 +1,113 @@
-import urllib.request
-import urllib.error
+import requests
 import json
 import time
+import os
+import atexit
 
-rps = 0.71  # max possible requests per second (rps) allowed by public API
-file_name = 'files/repos'
+api_limiter = (60 ** 2) / 5001.0  # max possible requests per hour allowed by Authenticated API
+file_name = 'files/repos.json'
+credentials_file = 'credentials.json'
 
-def retrieve_repo(since):
-    time.sleep(rps)
-    with urllib.request.urlopen(f'https://api.github.com/repositories?since={since}') as f:
-        content = f.read().decode('utf-8')
-        repo_list = json.loads(content)
-        return repo_list
-
-
-def retrieve_languages(name):
-    time.sleep(rps)
-    with urllib.request.urlopen(f'https://api.github.com/repos/{name}/languages') as f:
-        content = f.read().decode('utf-8')
-        language_list = json.loads(content)
-        return language_list
+access_token = 'b54f2b467f6b50145bee1b1c05b0395bf954db2f'
+repos_crawled = 0
+start_with = 185000000  # ensure we take new repos
+t = time.time()
 
 
-def retrieve_commits(name):
-    time.sleep(rps)
-    with urllib.request.urlopen(f'https://api.github.com/repos/{name}/commits') as f:
-        content = f.read().decode('utf-8')
-        commits_list = json.loads(content)
-        return list(map(lambda commit: commit['commit']['message'], commits_list))
+class CrawlerException(Exception):
+    pass
 
 
-def crawl_repos():
-    repos_crawled = 0
-    crawl_goal = 5000  # max possible in one hour
+def wait():
+    global t
+    sleep = api_limiter - (time.time() - t)
+    print(sleep)
+    print(wait)
+    time.sleep(sleep)
+    t = time.time()
 
-    with open(file_name, 'w') as file:
-        file.write('[\n')
 
-    while repos_crawled < crawl_goal:
-        try:
-            repos = retrieve_repo(repos_crawled)
-            for name in list(map(lambda repo: repo['full_name'], repos)):
-                print(f'Retrieving repo {name} ...')
-                langs = retrieve_languages(name)
-                commits = retrieve_commits(name)
-                repo_dict = {'repository': name,
-                             'languages': langs,
-                             'commits': commits}
-                repos_crawled += 1
-                with open(file_name, 'a+') as file:
-                    if repos_crawled > 1:
-                        file.write(',\n')
-                    file.write(json.dumps(repo_dict))
-        except urllib.error.HTTPError:
-            print('Public API limits exhausted')
-            break
-
+def close_file():
+    global repos_crawled
+    print(f'Stopping crawler after {repos_crawled} repos')
     with open(file_name, 'a+') as file:
         file.write('\n]')
 
 
+def make_request(url):
+    r = requests.get(url, auth=basicAuthCredentials)
+    if r.status_code > 400:
+        raise CrawlerException('Query failed with message: ' + r.text)
+    return json.loads(r.text)
+
+
+def retrieve_repo(since):
+    repo_list = make_request(f'https://api.github.com/repositories?since={since}')
+    return repo_list
+
+
+def retrieve_languages(name):
+    language_list = make_request(f'https://api.github.com/repos/{name}/languages')
+    return language_list
+
+
+def retrieve_commits(name):
+    commits_list = make_request(f'https://api.github.com/repos/{name}/commits')
+    return list(map(lambda commit: commit['commit']['message'], commits_list))
+
+
+def crawl_repos():
+    global repos_crawled
+    global basicAuthCredentials
+    crawl_goal = 20000  # would run for 4 hours
+
+    try:
+        os.mkdir('files')
+    except OSError:
+        pass
+
+    with open(file_name, 'w') as file:
+        file.write('[\n')
+
+    with open(credentials_file, 'r') as cred:
+        credentials = json.load(cred)
+        username = credentials['username']
+        password = credentials['password']
+        if not username or not password:
+            print('Please enter your GitHub credentials in the file credentials.json to start crawling.')
+            exit()
+        basicAuthCredentials = (username, password)
+
+    t = time.time()
+    while repos_crawled < crawl_goal:
+        try:
+            wait()
+            repos = retrieve_repo(start_with + repos_crawled)
+            for name in list(map(lambda repo: repo['full_name'], repos)):
+                print(f'Retrieving repo {name} ...')
+                repos_crawled += 1
+
+                wait()
+                langs = retrieve_languages(name)
+
+                wait()
+                commits = retrieve_commits(name)
+
+                repo_dict = {'repository': name,
+                             'languages': langs,
+                             'commits': commits}
+                with open(file_name, 'a+', encoding='utf8') as file:
+                    if repos_crawled > 1:
+                        file.write(',\n')
+                    file.write(json.dumps(repo_dict, ensure_ascii=False))
+            time.sleep(wait)
+        except CrawlerException as e:
+            repos_crawled += 1
+            print(e)
+
+
 def main():
+    atexit.register(close_file)
     crawl_repos()
 
 
--- feature_helper.py
+++ feature_helper.py
@@ -1,12 +1,28 @@
 import unicodedata
 import json
+import re
 
 
 def process_commits(commits):
     good_commits = []
     for commit in commits:
+        # filter out invalid commits
+        if len(re.findall(r'[\u4e00-\u9fff]+', commit)) > 0:
+            continue  # don't include commits with chinese characters
+        if len(re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+                          commit)) > 0:
+            continue  # don't include commits with links
+        if len(re.findall(r'\S+@\S+.', commit)) > 0:
+            continue  # don't include commits with e-mail addresses
+        if 'asd' in commit or 'initial commit' in commit:
+            continue
+
         commit = commit.strip()  # remove trailing spaces
         commit = "".join(ch for ch in commit if unicodedata.category(ch)[0] != "C")  # remove control characters
+
+        if len(commit) < 5:
+            continue  # don't include very short commit messages
+
         good_commits.append(commit)
     return good_commits
 
@@ -17,7 +33,6 @@ def get_relevant_langs(languages):
     num_languages = len(languages.keys())
     for lang in languages.keys():
         total += languages[lang]
-    print(total, num_languages)
     for lang in languages.keys():
         if languages[lang] >= total / num_languages:
             relevant_languages.append(lang)
@@ -25,19 +40,28 @@ def get_relevant_langs(languages):
 
 
 def main():
+    unique_repos = []
     features = []
     labels = []
-    with open('files/repos') as file:
+    with open('files/repos.json', encoding='utf8') as file:
         repos = json.load(file)
         for repo in repos:
             name = repo["repository"]
+            if name in unique_repos:
+                continue
+            else:
+                unique_repos.append(name)
+
             languages = get_relevant_langs(repo["languages"])
             commits = process_commits(repo["commits"])
 
-            print(f'Name: {name}')
-            print(f'Languages: {languages}')
-            print(f'Commits: {commits}')
-            print()
+            # print(f'Name: {name}')
+            # print(f'Languages: {languages}')
+            # print(f'Commits: {commits}')
+            # print()
+
+            if len(languages) == 0 or len(commits) < 5:
+                continue
 
             for commit in commits:
                 features.append(commit)
@@ -46,13 +70,13 @@ def main():
     if len(features) != len(labels):
         print('Unequal amount of features and labels. Something went horribly wrong')
 
-    print(f'{len(features)} feature extracted: {features}')
-    print(f'{len(labels)} feature extracted: {labels}')
+    print(f'{len(features)} feature extracted')
+    print(f'{len(labels)} feature extracted')
 
-    with open('files/features', 'w') as file:
+    with open('files/features.json', 'w') as file:
         file.write(json.dumps(features))
 
-    with open('files/labels', 'w') as file:
+    with open('files/labels.json', 'w') as file:
         file.write(json.dumps(labels))
 
 
